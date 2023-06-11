232f2569a27937f26043ff81eeb8d4b08607ac89
boburjon <boburr375@gmail.com>
2023-06-11 13:12:37 -0700

    mana

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,11 +8,83 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final bool _islogin=false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold(body: Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/im_party.jpeg'),
+          fit: BoxFit.cover,
 
-      
+        ),
+
+      ),
+      child: Container(padding:const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                colors: [
+                  Colors.black.withOpacity(.6),
+                  Colors.black.withOpacity(.5),
+                  Colors.black.withOpacity(.4),
+                  Colors.black.withOpacity(.1),
+                ]
+            )
+        ),
+        child:  Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children:  [
+
+
+          const Text(
+            "Find the best parties near you.",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                ),
+          ),
+            const SizedBox(height: 30),
+            const Text(
+              "Let us find you a party for your interest",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.w100),
+            ),
+            const SizedBox(height: 30,),
+            _islogin?
+            Container(
+              height: 60,
+              decoration: BoxDecoration(
+                color: Colors.yellow[900],
+                borderRadius: BorderRadius.circular(50)
+
+              ),
+              child: const Center(
+                child: Text('Start',style: TextStyle(fontSize: 18,color: Colors.white)),
+              ),
+            ):
+
+
+            Container(
+              height: 60,
+              decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(50)
+
+              ),
+              child: const Center(
+                child: Text("Google+",style: TextStyle(fontSize: 18,color: Colors.white)),
+              ),
+            ),
+            const SizedBox(height: 30,),
+      ]),
+    ),
+    )
     );
+
   }
 }
